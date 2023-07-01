4423bda3098abe03bfe97954c1ae40e85d67c017
reinerterig <54500223+reinerterig@users.noreply.github.com>
2023-07-01 20:08:06 +1000

    firs commit

---
+++ BrewSDK/BrewSDK.h
@@ -0,0 +1,18 @@
+//
+//  BrewSDK.h
+//  BrewSDK
+//
+//  Created by reinert wasserman on 1/7/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BrewSDK.
+FOUNDATION_EXPORT double BrewSDKVersionNumber;
+
+//! Project version string for BrewSDK.
+FOUNDATION_EXPORT const unsigned char BrewSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrewSDK/PublicHeader.h>
+
+
